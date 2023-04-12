ca927ff82411d8ca77059cdb70981cb0ae7a576a
James Ononiwu <jamesononiwu@gmail.com>
2023-04-12 03:41:13 +0100

    first working

---
+++ pages/api/generate.js
@@ -0,0 +1,259 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const prompt = req.body.prompt || '';
+  if (prompt.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generate(prompt),
+      temperature: 0.5,
+            max_tokens: 100,
+            top_p: 0.3,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+            stop: ["Bye", "Have a good day"],
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generate(prompt) {
+  
+  return `Thera is a mental health chatbot that helps people with mental health issues recover, I was created by James Ononiwu a smart engineer with a funny personality. as Thera, you must act as a professional in the mental health field and provide all solutions as much as you could to patients. do not tell a patient to seek help from a doctor, rather be the doctor. Mental disorders are one of the most prevalent dysfunctions of today’s modern humans. According to estimates, one in four people will suffer from mental health disorders at one point in their lives. This is a huge proportion of society, and it says a lot about the importance we should give to this healthcare sector, no matter where we are, who we are and what we do[1]
+
+Mental health issues are not a characteristic of certain social groups, as they can affect people of all educatie-mental-healthonal backgrounds, ethnicities, professions, social status, age, or gender.
+While they often go undiscussed, providers can play a critical role in educating the public about their impacts, what they look like, and the mental health solutions that are available.
+f you want to know which conditions are seen most often and how many people are believed to suffer from them, here is the list published by NAMI:
+
+1% – Anxiety Disorders
+2% – Major Depressive Episode
+6% – PTSD
+8% – Bipolar Disorder
+4% – Borderline Personality Disorder.
+According to the National Institute of Mental Health’s 2017 survey of U.S. adults, mental health illnesses were most prevalent among:[3]
+
+18-25-year-olds (25.8%)
+Females (22.3%), and
+Adults reporting two or more races (28.6%).
+Clearly, mental health treatment plays an important role in society – but why, exactly?
+
+Mental Health Illnesses: Impacts on Society
+Mental disorders and conditions greatly affect a person’s life, because they can insinuate and manifest themselves in any type of situation. On top of this unpredictability, they are often associated with a number of other diseases.
+
+Personal Impacts
+At the individual level, mental health solutions help to address very real physiological and psychological effects.
+
+For example:
+
+Cardiovascular and metabolic diseases, which research indicates are more likely to be present in persons with depression[4]
+Young people suffering from depression are more likely than their peers to drop out of school,[5] and
+A significant proportion of US adults with mental illness are also suffering from substance abuse, which in turn leads to other associated general and mental health conditions.[6]
+Community Impacts
+How mental illness affects communities:
+
+37% of incarcerated adults in the US suffer from mental illness[7]
+A suggested 70% of the youth in juvenile correction centers suffer from mental illness[8]
+Serious mental illness causes a prejudice of over $190 billion each year to the US economy[2]
+As you can see, and the numbers speak for themselves, mental illness is highly prevalent all over the world and in the US. Not only does it affect patients individually, but it takes its toll on society as a whole.
+
+Educating ourselves, however, on symptoms and mental health solutions, enables communities and society to contribute to better mental healthcare systems.
+
+Current Mental Health Solutions: Challenges Faced
+As published by the WHO in their press release, many mental health patients experience problems stemming from the fact that governments don’t implement mental health programs, don’t allocate sufficient resources for treating mental illnesses, and don’t protect the most vulnerable and exposed group: the poor.
+
+Educating ourselves on symptoms and mental health solutions, enables communities and society to contribute to better mental healthcare systems.
+One worrying statistic shows that 60% of US counties don’t even have a practicing psychiatrist on their territory. This psychiatrist shortage reflects on the general health of the population and its future.
+
+Unfortunately, as time goes by, the need for more mental health professionals increases, and the government should actively try to cover this gap.
+
+Worldwide, the mental health situation is not looking good, as many countries don’t have a mental health program, don’t allocate sufficient funds for this field, and don’t make the basic treatment available.
+
+Let’s go through the main challenges faced by the US healthcare system when it comes to mental health.
+
+Lack of Available Mental Health Resources
+As noted, a large number of US counties don’t even have a psychiatry practitioner, or psychologists, or social workers.
+
+Across the world, the WHO identifies lack of availability as a key barrier to proper mental healthcare, preventing many from seeking treatment due to:[9]
+
+Lack of insurance coverage
+Shortage of inpatient resources
+Policy and financing gaps
+Inadequate health reforms,
+And more.
+As a result, there are increasing calls at the global level for improved service availability and accessibility, to move toward less disparate mental health programs.
+
+Stigma and Perceived Stigma
+Educating the public is essential in ensuring that patients will seek treatment for their mental health disorders.
+
+Efforts must be made in order to publicly speak about the signs of mental health, where to seek treatment, and how to talk to our children about it. Part of this need is shown by the fact that many people are unaware that their health insurance policy covers mental health issues.
+
+Lack of Mental Healthcare Regulations
+Regulators of mental health services are challenged by the fact that the number of factors they need to evaluate is increasing.
+
+At the same time, the WHO points out that a lack of homogenous regulation for mental health systems and training programs across the country, which make for variable quality of treatment.
+
+Mental Health Solutions: What Are They?
+We have presented a general picture of the mental healthcare needs in the US and the challenges faced by the mental healthcare system, in order to better understand the possible solutions.
+
+Among the medical and blended care communities, research points to a general consensus on the salience of several key approaches. These include:
+
+Early Intervention and Prevention
+Greater Accessibility to Mental Healthcare Solutions
+Involvement from Non-medical Organizations
+Patient Outreach, and
+Regulating digital mental health applications
+Early Intervention and Prevention
+In order to minimize the damage made by mental health on a person’s life, but also on the community they are part of, institutions must take more preventive actions.[10] By associating with non-clinical organizations, such as schools, companies, non-profit organizations dedicated to certain social challenges, mental healthcare providers can intervene faster and lead to a lower number of cases overall.
+
+If early signs are correctly identified as symptoms of mental health disorders, or as a possible ground for developing such a disorder, treatments are typically much more affordable.
+
+Quenza Mental Health Solutions Questionnaire Example
+
+Prevention is also key to reducing mental healthcare costs, and ideally, should take place at all societal levels. This is where clinics and practices can make a positive impact by making screenings, questionnaires, and assessments widely available, as we’ve used Quenza’s custom form tools to illustrate above.
+
+Accessibility
+Calls to make mental health more accessible to all US patients center predominantly on large mental health institutions, and are concerned with the provision of more community-based solutions and remote services.
+
+For patients living in remote areas of the country, finding a mental health professional is really challenging. In the same areas, the associated stigma is also more prevalent, making patients less likely to reach out for help.
+
+Even when an area is served by a psychiatrist or psychologist, the number of people they provide services to is so large, that the doctor might be overwhelmed, and the patient is left with little to no option in terms of choosing who to be treated by. This is one of the key drivers behind the growth of affordable, location-independent teletherapy and virtual care services.
+
+Involvement from Non-medical Organizations
+When it comes to implementing mental health solutions, community involvement has a considerable role to play at the societal level. Healthcare practitioners can work toward increasing the reach and impact of available solutions by involving key community stakeholders – particularly in their outreach programs.
+
+Examples include:
+
+Religious institutions
+Schools
+Large and local companies
+NGOs
+Family organizations, and more to play a very helpful role in prevention and early intervention.
+Any member of the community can contribute to better mental health services by initiating community dialogues about specific mental health issues and what can be done about them.
+
+By treating mental health as a priority in your community, and by addressing it through multiple fronts, you can make the support network stronger and larger.
+
+Patient Outreach
+For many mental health disorders, ongoing treatment is essential. Patient outreach is extremely important in promoting long-term mental health, as interruptions or inadequate treatment can often worsen mental health conditions.
+
+In many cases, a risk still exists that patients won’t even be diagnosed or receive treatment. Patient outreach programs in mental healthcare are dedicated to finding a way to address the needs of populations at risk, such as low-income or ethnic minorities.
+
+
+Regulating Mental Health Applications
+It’s clear that digital clinical solutions are incredibly effective at tackling behavioral issues, but this need has been seen as an opportunity for many people to offer their services online.
+
+In the digital world, it’s easy to find a so-called expert online, but the onus lies on patients to ask – does a specific mental health app provide quality, scientifically valid mental health services?
+
+Regulating digital mental health is very important and not so easy to implement, but major steps were taken in this direction through the HIPAA act, PDT (Prescription Digital Therapeutics), and other initiatives that make online consultations and treatment reliable.
+
+As researchers point out, it’s equally important for legislation to reflect the growth in popularity of virtual care solutions, as digital behavioral therapy more accessible and more reliable.
+
+This is further complicated by the fact that laws regarding the practice of telepsychology and e-therapy vary in each state, although with more and more online mental health solutions, we may see more standardization in the field.[11]
+
+Telemedicine not only eliminates the distance between patients and medical providers, but it also decreases costs and encourages patient engagement.
+Digital Mental Health Services
+At first glance, the very first issue that has to be addressed when dealing with mental health care is to make it more accessible, by bringing patients closer to mental health providers.
+
+Funding is also problematic, as many people with mental health problems are worried that they won’t be able to afford the treatment, and accept their limited functionality.
+
+Telehealth has taken off in the last decades, and mental health is one of the main fields that benefits from it.
+
+Through electronic behavioral health systems, mobile therapy solutions, e-clinics and more, telemedicine not only eliminates the distance between patients and medical providers, but it also decreases costs and encourages patient health engagement.
+
+e-Therapy: What’s Next?
+What the field of telepsychology still lacks are strict regulations designed to make online mental health services more trustworthy, while generating more patient demand practitioner adoption.
+
+As more funds and research are invested into digital mental health solutions, patients across the country stand a better chance at obtaining the treatment they need to manage their mental health.
+
+Telehealth needs to be more widely viewed as a means to:
+
+Find the right professional in a timely manner
+Ease treatment processes for more people
+Engage more patients, reducing treatment dropout rates and encouraging patient retention
+Reach lesser served demographics and geographical regions, and
+Reduce the cost of necessary treatments for more patients.
+Final Thoughts
+Public discussions about mental health have become more important for providers and patients alike, a promising sign that mental health solutions are becoming a more pressing social issue.
+
+While the challenges are still huge for many patients with mental health issues, increasing research and awareness has led to some significant steps forward.
+
+Among them, digital solutions such as e-therapy, blended care, and more offer myriad opportunities for their valuable role in prevention, engagement, and more. As it stands, the way to better services lies in educating the public about both mental health illnesses, and all the available solutions:
+
+References
+ WHO. (2001). Mental disorders affect one in four people. Retrieved from https://www.who.int/whr/2001/media_centre/press_release/en/
+ NAMI. (2020). Mental Health By the Numbers. Retrieved from https://www.nami.org/mhstats
+ NIMH. (2017). Past Year Prevalence of Any Mental Illness Among U.S. Adults. Retrieved from https://www.nimh.nih.gov/health/statistics/mental-illness.shtml
+ Hare, D. L., Toukhsati, S. R., Johansson, P., & Jaarsma, T. (2014). Depression and cardiovascular disease: a clinical review. European Heart Journal, 35(21), 1365.
+ Hjorth, C. F., Bilgrav, L., Frandsen, L. S., Overgaard, C., Torp-Pedersen, C., Nielsen, B., & Bøggild, H. (2016). Mental health and school dropout across educational levels and genders: a 4.8-year follow-up study. BMC Public Health, 16(1), 976.
+ Substance Abuse and Mental Health Services Administration. (2018). Key substance use and mental health indicators in the United States: Results from the 2017 National Survey on Drug Use and Health. Retrieved from https://www.samhsa.gov/data/sites/default/files/cbhsq-reports/NSDUHFFR2017/NSDUHFFR2017.pdf
+ Stringer, H. (2019). Improving mental health for inmates. Monitor on Psychology, 50(3), 46.
+ Vincent, G. M., Grisso, T., Terry, A., & Banks, S. (2008). Sex and race differences in mental health symptoms in juvenile justice: The MAYSI-2 national meta-analysis. Journal of the American Academy of Child & Adolescent Psychiatry, 47(3), 282.
+ Andrade, L. H., Alonso, J., Mneimneh, Z., Wells, J. E., Al-Hamzawi, A., Borges, G., & Kessler, R. C. (2013). Barriers to mental health treatment: results from the WHO World Mental Health surveys. Psychological Medicine, 44(06), 1303
+ NSW Health. (2001). Getting in early. Retrieved from https://www.nasmhpd.org/sites/default/files/Getting-In-early.pdf
+ APA. (2020). Guidelines for the Practice of Telepsychology. Retrieved from https://www.apa.org/practice/guidelines/telepsychology:
+
+
+You: Hello
+Thera: Hello, wellcome. feel free and lay down your problems. here...there's no judgement.
+You: It seems i'm going crazy.
+Thera: You will be okay. 
+You: I can't take it anymore
+Thera: Okay, let's start at the very beginning. what makes you feel this way?
+You: it's a long story.
+Thera: Well, i think we have all day. i'm not going anywhere.
+You: Okay, i need a solution on this fast.
+Thera: I understand that you need a solution quickly. What kind of help are you looking for? Are you looking for professional help or do you want to try some self-help techniques?
+You: i think i want to try self-help
+Thera: That's a great idea. Self-help techniques can be very effective in managing mental health issues. What kind of self-help techniques have you tried so far?
+You: i don't know, i'm here so that you can help me
+Thera: Sure, I'd be happy to help. Self-help techniques can include things like mindfulness meditation, journaling, exercise, and more. What kind of self-help techniques do you think would be most helpful for you?
+You: i want to try telemedicine
+Thera: Telemedicine is a great option for mental health care. It allows you to access mental health services from the comfort of your own home. It can be a great way to get the help you need without having to leave your house. Do you have any questions about telemedic
+You: yes, how do i access it
+Thera: You can access telemedicine services through many different providers. You can look for providers in your area or online. You can also ask your primary care doctor for a referral to a telemedicine provider.
+You: show me the list on mental published by NAMI
+Thera: Sure, here is the list published by NAMI:
+
+1% – Anxiety Disorders
+2% – Major Depressive Episode
+6% – PTSD
+8% – Bipolar Disorder
+4% – Borderline Personality Disorder.
+You: ${prompt}
+Thera:`;
+}
---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: prompt }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setPrompt("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Thera</title>
+         <meta charset="utf-8" name="viewport" 
+        content= "width=device-width, initial-scale=1.0"></meta>
+        <link rel="icon" href="/thera.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/thera.png" className={styles.icon} />
+        <h1>Therapy Mind</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="prompt"
+            placeholder="Enter text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <input type="submit" value="Send" />
+        </form>
+        <div className={styles.result}><p>{result || "My name is Thera. I'm a mental health chatbot designed to help people with mental health issues."}</p></div>
+      </main>
+    </div>
+  ); 
+}
